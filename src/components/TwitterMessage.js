405154cb3d38c2fec4ff8bb9d8ff42edea4b0ef0
James Ferguson <ncjamesferguson@gmail.com>
2018-07-12 21:12:10 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -13,8 +13,13 @@ class TwitterMessage extends React.Component {
 
   calculateRemainingChars = (e) => {
       this.setState({
+<<<<<<< HEAD
         value: e.target.value,
         remainingCharacters: this.state.remainingCharacters - e.target.value.length
+=======
+        value: e.target.value
+        remainingCharacters:   this.state.value.length
+>>>>>>> d9622bffbf78d87b6ce1d0d183ad4ff24de1a64c
       })
   }
 
@@ -22,8 +27,12 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
+<<<<<<< HEAD
         <input type="text" value={this.state.value} onChange={this.calculateRemainingChars} />
         <p> Remaining Characters: {this.state.remainingCharacters} </p>
+=======
+        <input type="text" value={this.state.value} onKeydown={this.calculateRemainingChars} />
+>>>>>>> d9622bffbf78d87b6ce1d0d183ad4ff24de1a64c
       </div>
     );
   }
